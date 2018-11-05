42159ee19a260704607a44af76be98e5d1db7aaa
11 <drudolph914@gmail.com>
2018-11-04 23:50:05 -0500

    adding colors module

--- colors.js
+++ colors.js
@@ -1,9 +1,43 @@
 class Colors{
+
+    static get RED(){
+        return "red";
+    }
+
     static get PINK(){
-        return "";
+        return "#E50053;";
+    }
+
+    static get ORANGE(){
+        return "orange";
+    }
+
+    static get YELLOW(){
+        return "yellow";
+    }
+
+    static get GREEN(){
+        return "green";
+    }
+
+    static get BLUE(){
+        return "blue";
+    }
+
+    static get PURPLE(){
+        return "purple";
+    }
+
+    static get BROWN(){
+        return "brown";
+    }
+
+    static get BLACK(){
+        return "black";
     }
 
     static get WHITE(){
         return "#F0F0F0";
     }
+
 }
