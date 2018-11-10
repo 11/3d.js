3802fe2c649efc7b39b0fb56a967f28a1172ddce
11 <drudolph914@gmail.com>
2018-11-09 21:54:12 -0500

    consolidating the vector classes to one file

---
+++ vector.js
@@ -0,0 +1,158 @@
+class Vector2{
+
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Static vector addition function that returns a new vector
+     */
+    static plus(vec1, vec2){
+        var x = vec1.x + vec2.x;
+        var y = vec1.y + vec2.y;
+        return new Vector2(x, y);
+    }
+
+    /**
+     * Static vector subtraction function that returns a new vector
+     */
+    static minus(vec1, vec2){
+        var x = vec1.x - vec2.x;
+        var y = vec1.y - vec2.y;
+        return new Vector2(x, y);
+    }
+
+    /**
+     * Static vector dot product function that returns a new vector
+     */
+    static dot(vec1, vec2){
+        var u = vec1.x * vec2.x;
+        var v = vec1.y * vec2.y;
+
+        return x + y;
+    }
+
+    /**
+     * Static vector cross product function that returns a new vector
+     */
+    static times(vec1, vec2){
+        var u = vec1.x * vec2.y;
+        var v = vec1.y * vec2.x;
+
+        return new Vector2(u, v);
+    }
+
+    /**
+     * Generic addition function that adds one vector into the other
+     * The goal of this function is to act as the += operator
+     */
+    add(vec2){
+        this.x += vec2.x;
+        this.y += vec2.y;
+    }
+
+    /**
+     * Generic addition function that adds one vector into the other
+     * The goal of this function is to act as the += operator
+     */
+    subtract(vec2){
+        this.x -= vec2.x;
+        this.y -= vec2.y;
+    }
+
+    cross(vec1, vec2){
+        var u = this.x * vec2.y;
+        var v = this.y * vec2.x;
+
+        this.x = u;
+        this.y = y;
+    }
+
+}
+
+
+class Vector3{
+
+    constructor(x, y, z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    /**
+     * Static vector addition function that returns a new vector
+     */
+    static plus(vec1, vec2){
+        var x = vec1.x + vec2.x;
+        var y = vec1.y + vec2.y;
+        var z = vec1.z + vec2.z;
+        return new Vector3(x, y, z);
+    }
+
+    /**
+     * Static vector subtraction function that returns a new vector
+     */
+
+    static minus(vec1, vec2){
+        var x = vec1.x - vec2.x;
+        var y = vec1.y - vec2.y;
+        var z = vec1.z - vec2.z;
+        return new Vector3(x, y, z);
+    }
+
+    /**
+     * Static vector dot product function that returns a new vector
+     */
+    static dot(vec1, vec2){
+        var x = vec1.x * vec2.x;
+        var y = vec1.y * vec2.y;
+        var z = vec1.z * vec2.z;
+
+        return x + y + z;
+    }
+
+    /**
+     * Static vector cross product function that returns a new vector
+     */
+    static times(vec1, vec2){
+        var x = (vec1.y * vec2.z) - (vec1.z * vec2.y);
+        var y = (vec1.z * vec2.x) - (vec1.x * vec2.z);
+        var z = (vec1.x * vec2.y) - (vec1.y * vec2.x);
+
+        return new Vector3(x, y, z);
+    }
+
+    /**
+     * Generic addition function that adds one vector into the other
+     * The goal of this function is to act as the += operator
+     */
+    add(vec2){
+        this.x += vec2.x;
+        this.y += vec2.y;
+        this.z += vec2.z;
+    }
+
+    /**
+     * Generic addition function that adds one vector into the other
+     * The goal of this function is to act as the += operator
+     */
+    subtract(vec2){
+        this.x -= vec2.x;
+        this.y -= vec2.y;
+        this.z -= vec2.z;
+    }
+
+    /**
+     * Converts the current instance of a Vector3 to the cross product of another Vector3
+     */
+    cross(vec2){
+        var u = (this.y * vec2.z) - (this.z * vec2.y);
+        var v = (this.z * vec2.x) - (this.x * vec2.z);
+        var w = (this.x * vec2.y) - (this.y * vec2.x);
+
+        this.x = u;
+        this.y = v;
+        this.z = w;
+    }
+}
